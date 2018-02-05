746ae83788a2380dc1f3699976a1940b9eac761e
Faisal Manzer <faisal_manzer@Faisals-MacBook-Air.local>
2018-02-05 18:53:27 +0530

    File Change

--- data/courses.php
+++ data/courses.php
@@ -21,7 +21,7 @@ class course extends sql
     $this->arr = array(
       "type" => 'card',
       "title" => 'Hi',
-      "img" => 'cap1.jpeg',
+      "ig" => 'cap1.jpeg',
       "cont" => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
 
     );
@@ -30,117 +30,119 @@ class course extends sql
   }
   function setData(){
 
-    $this->arr = array(
+    $this->json = json_encode($this->arr);
+  }
+}
+/*
+
+$this->arr = array(
+  array(
+    'type' => 'para',
+    'title' => '<h4>B.Sc Maths</h4>',
+    'cont' => ''
+  ),
+  array(
+    'type' => 'collapsible',
+    'options' => array(
+      'accordion' => false,
+      'active' => 0
+    ),
+    'items' => array(
       array(
-        'type' => 'para',
-        'title' => '<h4>B.Sc Maths</h4>',
-        'cont' => ''
+        'title' => 'Basic Info',
+        'cont' => '',
+        'eclass' => 'active',
+        'saru' => array(
+          array(
+            'type' => 'table',
+            'items' => array(
+              array(
+                '<b>Duration</b>',
+                '3 Years'
+              ),
+              array(
+                '<b>Total Seat</b>',
+                '50'
+              ),
+              array(
+                '<b>Reservation',
+                'Muslim (50) <br> OBC Muslim (30) <br> Muslim Women (34)'
+              )
+            )
+          ),
+          array(
+            'type' => 'para',
+            'title' => '',
+            'cont' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente, magnam.'
+          ),
+          array(
+            'type' => 'para',
+            'title' => 'Address',
+            'cont' => 'the map goes here'
+          )
+        )
       ),
       array(
-        'type' => 'collapsible',
-        'options' => array(
-          'accordion' => false,
-          'active' => 0
-        ),
-        'items' => array(
+        'title' => 'Examination',
+        'cont' => '',
+        'saru' => array(
           array(
-            'title' => 'Basic Info',
-            'cont' => '',
-            'eclass' => 'active',
-            'saru' => array(
+            'type' => 'table',
+            'items' => array(
               array(
-                'type' => 'table',
-                'items' => array(
-                  array(
-                    '<b>Duration</b>',
-                    '3 Years'
-                  ),
-                  array(
-                    '<b>Total Seat</b>',
-                    '50'
-                  ),
-                  array(
-                    '<b>Reservation',
-                    'Muslim (50) <br> OBC Muslim (30) <br> Muslim Women (34)'
-                  )
-                )
+                '<b>Exam</b>',
+                '3 Years'
               ),
               array(
-                'type' => 'para',
-                'title' => '',
-                'cont' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente, magnam.'
+                '<b>Opening Date</b>',
+                '29 Nov 2017'
               ),
               array(
-                'type' => 'para',
-                'title' => 'Address',
-                'cont' => 'the map goes here'
-              )
-            )
-          ),
-          array(
-            'title' => 'Examination',
-            'cont' => '',
-            'saru' => array(
-              array(
-                'type' => 'table',
-                'items' => array(
-                  array(
-                    '<b>Exam</b>',
-                    '3 Years'
-                  ),
-                  array(
-                    '<b>Opening Date</b>',
-                    '29 Nov 2017'
-                  ),
-                  array(
-                    '<b>Closing Date</b>',
-                    '3 March 2018'
-                  ),
+                '<b>Closing Date</b>',
+                '3 March 2018'
+              ),
 
-                  array(
-                    '<b>Form Date</b>',
-                    '&#x20b9; 300'
-                  ),
-                  array(
-                    '<b>Examination Pattern</b>',
-                    '<b>Objective</b> <br> 100 Marks <br> 100 Questions'
-                  )
-                )
+              array(
+                '<b>Form Date</b>',
+                '&#x20b9; 300'
               ),
               array(
-                'type' => 'para',
-                'title' => '',
-                'cont' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente, magnam.'
+                '<b>Examination Pattern</b>',
+                '<b>Objective</b> <br> 100 Marks <br> 100 Questions'
               )
             )
           ),
           array(
-            'title' => 'Study Material',
-            'cont' => '',
-            'saru' => array(
-              'type' => 'table',
-              'items' => array(
-                array(
-                  '<b>Sllabus</b>',
-                  '<a href="#">Download</a>'
-                ),
-                array(
-                  '<b>Previous Years</b>',
-                  '<a href="#">Download</a>'
-                ),
-                array(
-                  '<b>Sample Paper</b>',
-                  '<a href="#">Download</a>'
-                )
-              )
+            'type' => 'para',
+            'title' => '',
+            'cont' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente, magnam.'
+          )
+        )
+      ),
+      array(
+        'title' => 'Study Material',
+        'cont' => '',
+        'saru' => array(
+          'type' => 'table',
+          'items' => array(
+            array(
+              '<b>Sllabus</b>',
+              '<a href="#">Download</a>'
+            ),
+            array(
+              '<b>Previous Years</b>',
+              '<a href="#">Download</a>'
+            ),
+            array(
+              '<b>Sample Paper</b>',
+              '<a href="#">Download</a>'
             )
           )
         )
       )
-    );
-    $this->json = json_encode($this->arr);
-  }
-}
-//echo "All fine";
-$x = new course("/courses/bsc/");
+    )
+  )
+);
+
+*/
  ?>
--- data/faisal/connect.php
+++ data/faisal/connect.php
@@ -23,23 +23,31 @@ class sql
   private $host;
   private $port;
   protected $conn;
+  protected $is_conn = 0;
+  protected $conn_err= '';
   protected $err = "";
   protected $query = "";
   protected $insert_id = "";
   protected $result;
 
-  function __construct($user = "root", $pass = "root", $host = "localhost:8889", $db)
-  {
+  function __construct($db, $user = "root", $pass = "root", $host = "localhost:8889"){
     $this->user = $user;
     $this->password = $pass;
     $this->host = $host;
     $this->db = $db;
     $this->conn = new mysqli($servername, $username, $pass, $db);
-    //echo "conn";
+    if ($this->conn->connect_error) {
+      $this->conn_err = $this->conn->connect_error;
+      $this->is_conn = 0;
+    }
+    else{
+      $this->is_conn = 1;
+    }
   }
 
   protected function execute($sql){
-    
-    }
+    $this->result = $this->conn->query(htmlspecialchars($sql));
+  }
+
 }
 ?>
