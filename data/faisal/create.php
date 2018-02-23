24cc08841b60e0782ffee703967e08a7cd9e07fb
Faisal Manzer <faisal.manzer11@gmail.com>
2018-02-23 11:12:01 +0530

    Create Php

--- data/faisal/create.php
+++ data/faisal/create.php
@@ -7,21 +7,20 @@
             $sql = "CREATE TABLE course(
                       sno INT NOT NULL AUTO_INCREMENT,
                       name VARCHAR(50) NOT NULL,
-                      token VARCHAR(50) NOT NULL,
-                      jid VARCHAR(50),
-                      branch INT NOT NULL,
+                      id VARCHAR(100) NOT NULL UNIQUE,
+                      sup_branch VARCHAR(50) NOT NULL,
                       internal BOOLEAN NOT NULL DEFAULT  'FALSE',
                       external BOOLEAN NOT NULL 'FALSE',
+                      PRIMARY (sno)
 )";
     $this->execute($sql);
+    echo "courses: ".$this->result." err: ".$this->err;
     $sql = "CREATE TABLE basic(
-              sno INT NOT NULL AUTO_INCREMENT,
-              token VARCHAR(50) NOT NULL,
-              time INT,
-              seat INT,
+              id VARCHAR(100) NOT NULL UNIQUE,
+              duration VARCHAR(255),
+              seats VARCHAR(255),
               reservation VARCHAR(255),
-              branch VARCHAR(255),
-              intro LONGTEXT 
+              intro  
 )";
         }
     }
